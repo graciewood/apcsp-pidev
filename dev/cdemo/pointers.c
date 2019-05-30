a9ed758f1148148c88fef3ddb5f1c8e6b351634a
Gracie Wood <woodg4752@my.sduhsd.net>
2019-05-30 08:08:04 -0700

    create myproject directory

--- dev/cdemo/pointers.c
+++ dev/cdemo/pointers.c
@@ -18,19 +18,19 @@ int main()
 	printf("The address of a is %d\n", &a);
 
 	float d = 0.00; 
-  float* ptrtod = &d;
+        float* ptrtod = &d;
 	float e = 3.14; 
-  float* ptrtoe = &e;
+        float* ptrtoe = &e;
 	
-  printf("The value of d is %f\n", d);
+        printf("The value of d is %f\n", d);
 	printf("The address of d is %d\n", &d);
 	printf("The value of e is %f\n", e);
-  printf("The address of e is %d\n", &e);
+        printf("The address of e is %d\n", &e);
 	
-  float tmp = e;
+        float tmp = e;
 	e = d;
 	d = tmp;
 
-  printf("new d: %f\n", d);
+        printf("new d: %f\n", d);
 	printf("new e: %f\n", e);
 }
