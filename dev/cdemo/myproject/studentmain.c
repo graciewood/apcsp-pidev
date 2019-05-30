ee0ba2669bb4dfe187a11949031d495bc8bb2a8d
graciewood <50209647+graciewood@users.noreply.github.com>
2019-05-30 08:41:07 -0700

    Update studentmain.c

--- dev/cdemo/myproject/studentmain.c
+++ dev/cdemo/myproject/studentmain.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Student 
-{
-  char firstName[256];
-  char lastName[256];
-  int age;
-  int studentID;
-};
-
-void printStudent(struct Student* student)
-{
-  printf("Name: %s %s\n", student -> firstName, student -> lastName);
-  printf("  Age: %d\n", student -> age);
-  printf("  Student ID: %d\n", student -> studentID);
-}
-
 int main() 
 {
   int numStudent;
